20c0357a345ca783140ab846af9bb2e41f4d90d2
Hirosh Dabui <hirosh@dabui.de>
2023-10-29 12:03:31 +0100

    only testing clock period to 10MHz

--- src/config.tcl
+++ src/config.tcl
@@ -16,7 +16,7 @@ set ::env(PL_TARGET_DENSITY) 0.6
 
 # CLOCK_PERIOD - Increase this in case you are getting setup time violations.
 # The value is in nanoseconds, so 20ns == 50MHz.
-set ::env(CLOCK_PERIOD) "20"
+set ::env(CLOCK_PERIOD) "100"
 
 # Hold slack margin - Increase them in case you are getting hold violations.
 set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.1
